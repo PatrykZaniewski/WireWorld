e17fb6709694d4204d9886134bcdea13b3f3d909
varAttre <37926375+varAttre@users.noreply.github.com>
2018-05-16 11:31:01 +0200

    Scene change added

--- src/gui/controllers/EntryController.java
+++ src/gui/controllers/EntryController.java
@@ -3,16 +3,23 @@ package gui.controllers;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EntryController {
 
     @FXML
     public ComboBox<String> templateCB;
     public TextArea TitleTA;
-    @FXML
+    public Button nextButton;
     private Button quitButton;
 
     public EntryController()
@@ -26,11 +33,21 @@ public class EntryController {
         templateCB.getSelectionModel().select(0);
     }
 
-    public void onButtonAction(ActionEvent actionEvent) {
+    public void quitButtonAction(ActionEvent actionEvent) {
         Platform.exit();
     }
 
     public void onThemeChosen(ActionEvent actionEvent){
 
     }
+
+    public void nexButtonAction(ActionEvent actionEvent) throws IOException {
+        Parent SecondParent = FXMLLoader.load(getClass().getResource("/gui/fxml/Second.fxml"));
+
+        Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(new Scene(SecondParent));
+
+    }
+
 }
+
