a1da1603325d8fa31e828c8c1a606418d517eae1
Unknown <kajgore@vp.pl>
2018-05-31 00:23:57 +0200

    + changed the object and border select + added default values of size/amount of gen + wrapping of board + added alert when trying to get back from designer

--- src/gui/controllers/BoardDesignerGUI.java
+++ src/gui/controllers/BoardDesignerGUI.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 public class BoardDesignerGUI {
 
@@ -37,7 +38,6 @@ public class BoardDesignerGUI {
     public Button quitButton;
     public Button backBT;
     public Button generateBT;
-    public TextField fileTF;
     private int height = BoardSize.getHeight();
     private int width = BoardSize.getWidth();
     private boolean borderType = BoardSize.getBorder();
@@ -112,7 +112,7 @@ public class BoardDesignerGUI {
         if(Simulation.firstGen[0][0] == 5)
         {
             generationsTF.setText(String.valueOf(Simulation.getAmount()));
-            ChangeColor paneColor = new ChangeColor(playgroundTP);
+            ChangeColor paneColor = new ChangeColor(playgroundTP, borderType);
             for (int j = 1; j <= BoardSize.getHeight(); j++) {
                 for (int k = 1; k <= BoardSize.getWidth(); k++) {
                     if (Simulation.firstGen[j][k] == 1) paneColor.makeYellow(j-1, k-1);
@@ -143,50 +143,39 @@ public class BoardDesignerGUI {
 
     public void onAndAction(ActionEvent actionEvent) {
         if(andCB.isSelected())disableAll(andCB);
-        else enableAll();
+        else positionCoB.setDisable(true);
     }
 
     public void onOrAction(ActionEvent actionEvent) {
         if(orCB.isSelected())disableAll(orCB);
-        else enableAll();
+        else positionCoB.setDisable(true);
     }
 
     public void onDiodeAction(ActionEvent actionEvent) {
         if(diodeCB.isSelected())disableAll(diodeCB);
-        else enableAll();
+        else positionCoB.setDisable(true);
     }
 
     public void onBigGenAction(ActionEvent actionEvent) {
         if(bigGeneratorCB.isSelected())disableAll(bigGeneratorCB);
-        else enableAll();
+        else positionCoB.setDisable(true);
     }
 
     public void onSmallGenAction(ActionEvent actionEvent) {
         if(smallGeneratorCB.isSelected())disableAll(smallGeneratorCB);
-        else enableAll();
+        else positionCoB.setDisable(true);
     }
 
     private void disableAll(CheckBox notDisabled)
     {
-        andCB.setDisable(true);
-        orCB.setDisable(true);
-        diodeCB.setDisable(true);
-        bigGeneratorCB.setDisable(true);
-        smallGeneratorCB.setDisable(true);
+        smallGeneratorCB.setSelected(false);
+        andCB.setSelected(false);
+        orCB.setSelected(false);
+        diodeCB.setSelected(false);
+        bigGeneratorCB.setSelected(false);
 
-        notDisabled.setDisable(false);
+        notDisabled.setSelected(true);
         positionCoB.setDisable(false);
-    }
-
-    private void enableAll()
-    {
-        andCB.setDisable(false);
-        orCB.setDisable(false);
-        diodeCB.setDisable(false);
-        bigGeneratorCB.setDisable(false);
-        smallGeneratorCB.setDisable(false);
-
-        positionCoB.setDisable(true);
     }
 
     public void onQuitButton(ActionEvent actionEvent) {
@@ -194,15 +183,29 @@ public class BoardDesignerGUI {
     }
 
     public void onBackAction(ActionEvent actionEvent) throws IOException {
-        Simulation.firstGen[0][0] = 0;
-        playgroundTP.getChildren().removeAll();
-        Parent DesignerSceneParent = FXMLLoader.load(getClass().getResource("/gui/fxml/SelectionOfBoard.fxml"));
-        DesignerSceneParent.setStyle("-fx-background-color: " + Theme.getColorName());
-        Stage stage = new Stage();
-        stage.setScene(new Scene(DesignerSceneParent, 600, 400));
-        stage.setResizable(false);
-        stage.show();
-        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Ostrzeżenie");
+        alert.setHeaderText("");
+        alert.setContentText("Powrót do okna z rozmiarem spowoduje utratę aktualnej planszy.");
+
+        ButtonType buttonTypeGo = new ButtonType("Cofnij");
+        ButtonType buttonTypeCancel = new ButtonType("Anuluj", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonTypeGo, buttonTypeCancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeGo){
+            Simulation.firstGen[0][0] = 0;
+            playgroundTP.getChildren().removeAll();
+            Parent DesignerSceneParent = FXMLLoader.load(getClass().getResource("/gui/fxml/SelectionOfBoard.fxml"));
+            DesignerSceneParent.setStyle("-fx-background-color: " + Theme.getColorName());
+            Stage stage = new Stage();
+            stage.setScene(new Scene(DesignerSceneParent, 600, 400));
+            stage.setResizable(false);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }
     }
 
     public void onGenerateAction(ActionEvent actionEvent) throws IOException {
@@ -225,7 +228,7 @@ public class BoardDesignerGUI {
                 }
             }
             int amount = Integer.parseInt(generationsTF.getText());
-            Simulation simulation = new Simulation(Array, amount);
+            Simulation simulation = new Simulation(Array, amount, borderType);
             simulation.startSim();
             Parent DesignerSceneParent = FXMLLoader.load(getClass().getResource("/gui/fxml/Simulation.fxml"));
             DesignerSceneParent.setStyle("-fx-background-color: " + Theme.getColorName());
--- src/gui/logic/ChangeColor.java
+++ src/gui/logic/ChangeColor.java
@@ -8,6 +8,12 @@ public class ChangeColor {
 
     private Pane playgroundTP;
     private Rectangle r;
+    private boolean borderType;
+
+    public ChangeColor(Pane playgroundTP, boolean borderType) {
+        this.playgroundTP = playgroundTP;
+        this.borderType = borderType;
+    }
 
     public ChangeColor(Pane playgroundTP) {
         this.playgroundTP = playgroundTP;
@@ -15,23 +21,67 @@ public class ChangeColor {
 
     public void makeRed(int row, int column)
     {
-        r = (Rectangle) playgroundTP.getChildren().get( (row) * BoardSize.getWidth() + (column));
-        r.setFill(Color.RED);
+        if(checkBorder(row, column)) {
+            r = (Rectangle) playgroundTP.getChildren().get((row) * BoardSize.getWidth() + (column));
+            r.setFill(Color.RED);
+        }
+        else if (!borderType)
+        {
+            row = overRow(row);
+            column = overColumn(column);
+            r = (Rectangle) playgroundTP.getChildren().get((row) * BoardSize.getWidth() + (column));
+            r.setFill(Color.RED);
+        }
     }
     public void makeBlue(int row, int column)
     {
-        r = (Rectangle) playgroundTP.getChildren().get( (row) * BoardSize.getWidth() + (column));
-        r.setFill(Color.BLUE);
+        if(checkBorder(row, column)) {
+            r = (Rectangle) playgroundTP.getChildren().get((row) * BoardSize.getWidth() + (column));
+            r.setFill(Color.BLUE);
+        }
+        else if (!borderType)
+        {
+            row = overRow(row);
+            column = overColumn(column);
+            r = (Rectangle) playgroundTP.getChildren().get((row) * BoardSize.getWidth() + (column));
+            r.setFill(Color.BLUE);
+        }
     }
     public void makeYellow(int row, int column)
     {
-        r = (Rectangle) playgroundTP.getChildren().get( (row) * BoardSize.getWidth() + (column));
-        r.setFill(Color.YELLOW);
+        if(checkBorder(row, column)) {
+            r = (Rectangle) playgroundTP.getChildren().get((row) * BoardSize.getWidth() + (column));
+            r.setFill(Color.YELLOW);
+        }
+        else if (!borderType)
+        {
+            row = overRow(row);
+            column = overColumn(column);
+            r = (Rectangle) playgroundTP.getChildren().get((row) * BoardSize.getWidth() + (column));
+            r.setFill(Color.YELLOW);
+        }
     }
 
-    public boolean checkBorder (int row, int column)
+    private boolean checkBorder(int row, int column)
     {
         return row >= 0 && row < BoardSize.getHeight() && column >= 0 && column < BoardSize.getWidth();
+    }
+
+    private int overRow (int row)
+    {
+        if(row >= 0) {
+            return row % BoardSize.getHeight();
+        }
+        else
+            return (Math.abs(row) + BoardSize.getHeight()-2)  % BoardSize.getHeight();
+    }
+
+    private int overColumn (int column)
+    {
+        if(column >= 0)
+            return column % BoardSize.getWidth();
+        else
+            return (Math.abs(column) + BoardSize.getWidth()-2) % BoardSize.getWidth();
     }
 
 }
